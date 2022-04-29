b99d765039d17941606dc3bec52f48dcc01c93da
Allessandro Anjos <allessandro.anjos@gmail.com>
2022-04-29 10:43:32 -0300

    corrige database default

--- eventex/settings.py
+++ eventex/settings.py
@@ -84,10 +84,6 @@ default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
 }
 
 # Password validation
